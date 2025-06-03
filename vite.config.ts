1a1af6d3b864294654b0f151154eb08745be1fe7
JanisSeibutis <janis.seibutis@gmail.com>
2025-06-03 08:59:50 +0200

    chore: set up github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/fed24d-js-ext-fed24d-the-last-todos-fed24d-the-last-todos/",
 })
