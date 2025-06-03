4695d163b6b6a8f3ce8231c8e0b5c76afb3bc5e7
JanisSeibutis <janis.seibutis@gmail.com>
2025-06-03 09:15:36 +0200

    chore: update vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/fed24d-js-ext-fed24d-the-last-todos-fed24d-the-last-todos/",
+  base: "/fed24d-the-last-todos-JanisSeibutis/",
 })
