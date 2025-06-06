9572374db5b196d39bd173032e856522baa2d83d
JanisSeibutis <janis.seibutis@gmail.com>
2025-06-06 13:21:55 +0200

    feat: show created_at date on each todo

--- src/components/TodoDisplay.tsx
+++ src/components/TodoDisplay.tsx
@@ -11,9 +11,21 @@ type TodoProps = {
 export const TodoDisplay = ({ todo, todos, setTodos }: TodoProps) => {
   return (
     <>
-      <p className={`text-left  ${todo.isDone ? "done" : ""}`}>{todo.todo}</p>
-      <TodoIsDoneToggle todos={todos} todo={todo} setTodos={setTodos} />
-      <TodoDeleteBtn todos={todos} todo={todo} setTodos={setTodos} />
+      <span className="pb-2">
+        {new Date(todo.created_at).toLocaleString(undefined, {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        })}
+      </span>
+      <div className="flex flex-row gap-5">
+        <p className={`text-left  ${todo.isDone ? "done" : ""}`}>{todo.todo}</p>
+        <TodoIsDoneToggle todos={todos} todo={todo} setTodos={setTodos} />
+        <TodoDeleteBtn todos={todos} todo={todo} setTodos={setTodos} />
+      </div>
     </>
   )
 }
