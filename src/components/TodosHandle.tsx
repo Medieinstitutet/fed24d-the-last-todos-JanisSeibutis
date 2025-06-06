e05e7588e43864719dd970d70ee2ced28c4d968f
JanisSeibutis <janis.seibutis@gmail.com>
2025-06-06 11:33:52 +0200

    style: add styling to todos list

--- src/components/TodosHandle.tsx
+++ src/components/TodosHandle.tsx
@@ -1,5 +1,6 @@
 import type { Todo } from "../models/Todo"
-import { TodosDisplay } from "./TodosDisplay"
+import { TodoDisplay } from "./TodoDisplay"
+import { TodoSort } from "./TodoSort"
 
 type TodoProps = {
   todos: Todo[]
@@ -9,29 +10,37 @@ type TodoProps = {
 export const TodosHandle = ({ todos, setTodos }: TodoProps) => {
   return (
     <>
+      <TodoSort todos={todos} setTodos={setTodos} />
+
       {/* Todos list */}
-      <div className="todo-list">
-        <ul>
+      <div className="min-w-full  p-1">
+        <ul className="flex flex-col gap-3">
           {todos
             .filter((t) => !t.isDone)
             .map((t) => (
-              <li key={t.id}>
-                <TodosDisplay todo={t} todos={todos} setTodos={setTodos} />
+              <li
+                key={t.id}
+                className="flex flex-row gap-3 sm:text-lg  text-sm border-b-2 p-3"
+              >
+                <TodoDisplay todo={t} todos={todos} setTodos={setTodos} />
               </li>
             ))}
         </ul>
       </div>
 
-      <h2>Completed todos</h2>
+      {todos.some((t) => t.isDone) && <h2 className="mt-4">Completed todos</h2>}
 
       {/* Completed Todos list */}
-      <div className="completed-todo-list">
+      <div className="completed-todo-list min-w-full p-1">
         <ul>
           {todos
             .filter((t) => t.isDone)
             .map((t) => (
-              <li key={t.id}>
-                <TodosDisplay todo={t} todos={todos} setTodos={setTodos} />
+              <li
+                key={t.id}
+                className="flex flex-row gap-3 sm:text-lg  text-sm border-b-2 p-3"
+              >
+                <TodoDisplay todo={t} todos={todos} setTodos={setTodos} />
               </li>
             ))}
         </ul>
